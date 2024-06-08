01edcc05a67795863f911b76ef25190e172da977
musketour <138710548+musketour@users.noreply.github.com>
2024-06-07 20:54:51 -0700

    Update Movie.java

--- src/main/java/com/librarymanagement/demo/main/Movie.java
+++ src/main/java/com/librarymanagement/demo/main/Movie.java
@@ -1,2 +1,28 @@
-package com.librarymanagement.demo.main;public class Movie {
+package com.librarymanagement.demo.main;
+
+public class Movie extends Item {
+    private String director;
+    private int duration;
+
+    public Movie(int id, String title, int publishedYear, boolean availability, String director, int duration) {
+        super(id, title, publishedYear, availability);
+        this.director = director;
+        this.duration = duration;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
 }

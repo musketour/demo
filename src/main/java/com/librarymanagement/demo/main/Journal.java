cc19143aaa01b20fdb626183d0ade5bfdce6d271
musketour <138710548+musketour@users.noreply.github.com>
2024-06-07 20:54:40 -0700

    Update Journal.java

--- src/main/java/com/librarymanagement/demo/main/Journal.java
+++ src/main/java/com/librarymanagement/demo/main/Journal.java
@@ -1,2 +1,28 @@
-package com.librarymanagement.demo.main;public class Journal {
+package com.librarymanagement.demo.main;
+
+public class Journal extends Item {
+    private String editor;
+    private String ISSN;
+
+    public Journal(int id, String title, int publishedYear, boolean availability, String editor, String ISSN) {
+        super(id, title, publishedYear, availability);
+        this.editor = editor;
+        this.ISSN = ISSN;
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+
+    public String getISSN() {
+        return ISSN;
+    }
+
+    public void setISSN(String ISSN) {
+        this.ISSN = ISSN;
+    }
 }
